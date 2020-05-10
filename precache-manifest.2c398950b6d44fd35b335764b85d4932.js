a469fd63fcc692c3339ee280a9988fbe23e8b159
jarubio34 <jarubio34@nisena.edu.co>
2020-05-09 22:59:27 -0500

    First commit

---
+++ precache-manifest.2c398950b6d44fd35b335764b85d4932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13aa277ab374b972bb75ec21d959dbae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06d0c7778d39533fedf5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3c0baa234a96c4070152",
+    "url": "/static/js/2.a6e110a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a6e110a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d0c7778d39533fedf5",
+    "url": "/static/js/main.56b3f1fe.chunk.js"
+  },
+  {
+    "revision": "0228ec8a8d1ea04aa7e1",
+    "url": "/static/js/runtime-main.e0709d1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
